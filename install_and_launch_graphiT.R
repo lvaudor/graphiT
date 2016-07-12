4060ede72ae09786418d80876a86352628c44c65
Lise Vaudor <lise.vaudor@ens-lyon.fr>
2016-07-12 15:39:13 +0200

    add all dependencies

--- install_and_launch_graphiT.R
+++ install_and_launch_graphiT.R
@@ -3,11 +3,17 @@ setwd("..")
 
 devtools::document("graphiT")
 
+require(tools)
+deps<- package_dependencies(packages = c("shiny","ggplot2"), pdb,
+                            which = c("Imports","Depends"),
+                            recursive = TRUE, reverse = FALSE)
+deps=as.vector(unlist(deps))
+deps=paste0(deps, collapse=",")
+
+
 #devtools::use_vignette("graphiT_vignette",pkg="graphiT")
 devtools::install("graphiT")
 
 require(graphiT)
 #devtools::load_all("graphiT")
 graphiT()
-
-#build("graphiT",binary=TRUE
